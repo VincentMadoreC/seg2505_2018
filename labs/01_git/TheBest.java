58ceed106881f8fe9f0c5549f536e8f385043966
VincentMadoreC <32103798+VincentMadoreC@users.noreply.github.com>
2018-09-11 18:55:03 -0400

    modified TheBest.java

--- labs/01_git/TheBest.java
+++ labs/01_git/TheBest.java
@@ -2,6 +2,16 @@
 public class TheBest {
     
     public static void main(String[] args) {
+        PersonInfo1.displayFr();
+        PersonInfo2.displayFr();
+        PersonInfo3.displayFr();
+        PersonInfo4.displayFr();
+        PersonInfo5.displayFr();
 
+        PersonInfo1.displayEn();
+        PersonInfo2.displayEn();
+        PersonInfo3.displayEn();
+        PersonInfo4.displayEn();
+        PersonInfo5.displayEn();
     }
 }
